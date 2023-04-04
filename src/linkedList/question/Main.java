2ca59e0eb65d9def81aeb2e202d19b216721f40a
sum7615 <sum793763@gmail.com>
2023-04-05 00:05:00 +0530

    Partition linkedlist time -> O(N)

--- src/linkedList/question/Main.java
+++ src/linkedList/question/Main.java
@@ -4,63 +4,112 @@ import java.util.HashSet;
 
 public class Main {
 	public static void main(String[] args) {
-		SinglyLinkedList sll = new SinglyLinkedList();
-		sll.create(1);
-		sll.insert(2);
-		sll.insert(3);
-		sll.insert(4);
-//		sll.insert(2);
+//		SinglyLinkedList sll = new SinglyLinkedList();
+//		
+//		
+//		sll.create(3);
 //		sll.insert(5);
-//		sll.insert(3);
-		sll.traversal();
+//		sll.insert(8);
+//		sll.insert(5);
+//		sll.insert(10);
+//		sll.insert(2);
+//		sll.insert(1);
+//		sll.traversal();
 //		deleteDuplicate(sll);
 //		deleteDuplicateOptimised(sll);
-		System.out.println(nthValueFromLastNode(sll,2)); 
+//		System.out.println(nthValueFromLastNode(sll,2)); 
 //		sll.traversal();
+//
+//		SinglyLinkedList ll = partition(sll, 5);
+//		ll.traversal();
 
+		SinglyLinkedList sll = new SinglyLinkedList();
+		SinglyLinkedList sll1 = new SinglyLinkedList();
+		
+		
+		sll.create(3);
+		sll.insert(5);
+		sll.insert(8);
+		
+		sll1.create(10);
+		sll1.insert(2);
+		sll1.insert(1);
+		sll1.traversal();
+		
+		
+		
 	}
 //	Delete Duplicate entries
 
 //	O(N^2)
 	public static void deleteDuplicate(SinglyLinkedList sll) {
-		Node temp =sll.head;
-		while(temp!=null) {
+		Node temp = sll.head;
+		while (temp != null) {
 			Node secondNode = temp;
-			while(secondNode.next!=null) {
-				if(secondNode.next.value==temp.value) {
-					secondNode.next=secondNode.next.next;
+			while (secondNode.next != null) {
+				if (secondNode.next.value == temp.value) {
+					secondNode.next = secondNode.next.next;
 					sll.size--;
-				}else {
-					secondNode=secondNode.next;
+				} else {
+					secondNode = secondNode.next;
 				}
 			}
-			temp=temp.next;
+			temp = temp.next;
 		}
 	}
+
 //	Time: O(N), Space:O(N)
 	public static void deleteDuplicateOptimised(SinglyLinkedList sll) {
 		HashSet<Integer> set = new HashSet<Integer>();
 		Node temp = sll.head;
-		Node prev =null;
-		while(temp!=null) {
-			if(set.contains(temp.value)&&prev!=null) {
-				prev.next=temp.next;
+		Node prev = null;
+		while (temp != null) {
+			if (set.contains(temp.value) && prev != null) {
+				prev.next = temp.next;
 				sll.size--;
-			}else {
+			} else {
 				set.add(temp.value);
-				prev=temp;
+				prev = temp;
 			}
-			temp=temp.next;
+			temp = temp.next;
 		}
 	}
-	
-	public static int nthValueFromLastNode(SinglyLinkedList sll,int n) {
+
+	public static int nthValueFromLastNode(SinglyLinkedList sll, int n) {
 		Node node = sll.head;
-		for(int i=0;i<sll.size-n-1;i++) {
-			node=node.next;
+		for (int i = 0; i < sll.size - n - 1; i++) {
+			if (i > sll.size) {
+				return -1;
+			}
+			node = node.next;
 		}
 		return node.value;
-		
+
+	}
+//Partition of linkedlist time: O(N)
+	public static SinglyLinkedList partition(SinglyLinkedList sll, int n) {
+		Node node = sll.head;
+		for (int i = 0; i < sll.size; i++) {
+			if (node.value >= n) {
+				Node next  = node.next;
+				sll.tail.next = node;
+				sll.tail = node;
+				sll.tail.next = null;
+				node = next;
+
+			} else {
+					if(i==0) {
+						node=node.next;
+					}else {
+						Node next  = node.next;
+			            node.next=sll.head;
+			            sll.head = node;
+			            node =next;
+					}
+					
+			}
+		}
+		return sll;
 	}
 
 }
