f56b73174eee5a00bcebd190ba0042a4ebce0e4a
sum7615 <sum793763@gmail.com>
2023-04-05 14:16:16 +0530

    Sum time complexity O(N)

--- src/linkedList/question/Main.java
+++ src/linkedList/question/Main.java
@@ -25,20 +25,62 @@ public class Main {
 
 		SinglyLinkedList sll = new SinglyLinkedList();
 		SinglyLinkedList sll1 = new SinglyLinkedList();
-		
-		
+
 		sll.create(3);
 		sll.insert(5);
 		sll.insert(8);
-		
-		sll1.create(10);
+
+		sll1.create(8);
 		sll1.insert(2);
 		sll1.insert(1);
-		sll1.traversal();
-		
-		
-		
+//		sll1.traversal();
+
+//		int kk = numberFormation(sll1);
+//		System.out.println(kk);
+
+		SinglyLinkedList res = sum(sll, sll1);
+		res.traversal();
+
+
 	}
+
+	public static SinglyLinkedList sum(SinglyLinkedList sl, SinglyLinkedList sl1) {
+		int num1 = numberFormation(sl);
+		int num2 = numberFormation(sl1);
+		int sum = num1 + num2;
+		SinglyLinkedList result = new SinglyLinkedList();
+		while (sum > 0) {
+			int n = sum % 10;
+			Node node = new Node();
+			node.value = n;
+			if (result.head == null) {
+				result.head = node;
+				result.tail = node;
+			} else {
+				node.next = result.head;
+				result.head = node;
+			}
+			sum /= 10;
+		}
+		return result;
+
+	}
+
+	// Number formation in reverse order
+	public static int numberFormation(SinglyLinkedList sl) {
+		return reverseTraversal(sl.head);
+	}
+
+	public static int reverseTraversal(Node node) {
+		if (node == null) {
+			
+			return 0;
+		}
+		int num = reverseTraversal(node.next);
+		num = (num * 10) + node.value;
+		return num;
+	}
+
 //	Delete Duplicate entries
 
 //	O(N^2)
@@ -86,27 +128,28 @@ public class Main {
 		return node.value;
 
 	}
+
 //Partition of linkedlist time: O(N)
 	public static SinglyLinkedList partition(SinglyLinkedList sll, int n) {
 		Node node = sll.head;
 		for (int i = 0; i < sll.size; i++) {
 			if (node.value >= n) {
-				Node next  = node.next;
+				Node next = node.next;
 				sll.tail.next = node;
 				sll.tail = node;
 				sll.tail.next = null;
 				node = next;
 
 			} else {
-					if(i==0) {
-						node=node.next;
-					}else {
-						Node next  = node.next;
-			            node.next=sll.head;
-			            sll.head = node;
-			            node =next;
-					}
-					
+				if (i == 0) {
+					node = node.next;
+				} else {
+					Node next = node.next;
+					node.next = sll.head;
+					sll.head = node;
+					node = next;
+				}
+
 			}
 		}
 		return sll;
