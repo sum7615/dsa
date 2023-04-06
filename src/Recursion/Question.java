42c79b23ca3957fef11388a40d8b62b27e158e23
sum7615 <sum793763@gmail.com>
2023-04-06 11:53:17 +0530

    Recursion find factorial. Time complexity O(N)

--- src/Recursion/Question.java
+++ src/Recursion/Question.java
@@ -2,13 +2,21 @@ package Recursion;
 
 public class Question {
 	public static void main(String[] args) {
-System.out.println(power(2,3));
+//		System.out.println(power(2, 3));
+		System.out.println(factorial(4));
 	}
+
 	public static int power(int base, int exception) {
-		if(exception==0) {
+		if (exception == 0) {
 			return 1;
 		}
-		return base*power(base,exception-1);
-		
+		return base * power(base, exception - 1);
+	}
+
+	public static int factorial(int num) {
+		if (num == 0) {
+			return 1;
+		}
+		return num * factorial(num - 1);
 	}
 }
