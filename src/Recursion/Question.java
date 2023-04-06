5b7d84a50f1fccf43c267e1cf4b7e94841e372f6
sum7615 <sum793763@gmail.com>
2023-04-06 11:59:14 +0530

    Recursion: product of array. Time complexity O(N)

--- src/Recursion/Question.java
+++ src/Recursion/Question.java
@@ -3,7 +3,9 @@ package Recursion;
 public class Question {
 	public static void main(String[] args) {
 //		System.out.println(power(2, 3));
-		System.out.println(factorial(4));
+//		System.out.println(factorial(4));
+		int[] arr= {1,2,3,4};
+		System.out.println(productofArray(arr,arr.length));
 	}
 
 	public static int power(int base, int exception) {
@@ -18,5 +20,13 @@ public class Question {
 			return 1;
 		}
 		return num * factorial(num - 1);
+	}
+
+	public static int productofArray(int A[], int N) {
+		if(N==0) {
+			return 1;
+		}
+		return A[N-1]*productofArray(A, N-1);
+		
 	}
 }
