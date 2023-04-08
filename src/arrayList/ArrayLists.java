0f9ce9f99a8209708df312326034a4bf50a140e0
sum7615 <sum793763@gmail.com>
2023-04-08 21:48:25 +0530

    Arraylist insertion by add(element) time and space O(N) and by add(index,element) where time O(N), space O(1)

--- src/arrayList/ArrayLists.java
+++ src/arrayList/ArrayLists.java
@@ -6,6 +6,9 @@ import java.util.Arrays;
 
 public class ArrayLists {
 	public static void main(String[] args) {
+		
+//		Creating ArrayList: Time and space complexity O(1)
+		
 //		Heterogenious data type
 		ArrayLists arrayList = new ArrayLists();
 		
@@ -15,6 +18,19 @@ public class ArrayLists {
 		
 		ArrayList<Integer> numbers = new ArrayList<Integer>(Arrays.asList(32,12,3,5432));
 		System.out.println(numbers);
+		
+//		intserting at Array List
+		
+//		using add(element) method. Time and space complexity is O(1)
+		intArrayList.add(1);
+		intArrayList.add(2);// insertig at the end
+		
+//		using add(index,element) method. Time complexity is ON1) and space complexity is O(1)
+		intArrayList.add(2,3); //inserting at 2 index. at the last of the array.
+		
+//		intArrayList.add(4,4);     //inserting at the index which is greater then size. so it will give exception 
+		
+		
 		
 	}
 }
