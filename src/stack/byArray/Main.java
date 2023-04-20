e42c6dcc41dad8fa03bddb3a5d2e00d11ad51762
sum7615 <sum793763@gmail.com>
2023-04-20 22:18:55 +0530

    Stack

---
+++ src/stack/byArray/Main.java
@@ -0,0 +1,9 @@
+package stack.byArray;
+
+public class Main {
+	public static void main(String[] args) {
+		Stack stack = new Stack(4);
+		stack.push(1);
+		
+	}
+}
---
+++ src/stack/byArray/Stack.java
@@ -0,0 +1,61 @@
+package stack.byArray;
+
+public class Stack {
+	int topOfStack;
+	int[] arr;
+
+	public Stack(int size) {
+		arr = new int[size];
+		topOfStack = -1;
+	}
+
+	public boolean isFull() {
+		if (topOfStack == arr.length - 1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean isEmpty() {
+		if (topOfStack == -1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void push(int value) {
+		if (isFull()) {
+			System.out.println("Stack is full");
+		} else {
+			arr[topOfStack] = value;
+			topOfStack++;
+		}
+
+	}
+
+	public int pop() {
+		if (isEmpty()) {
+			System.out.println("The stack is empty");
+			return -1;
+		} else {
+			int res = arr[topOfStack];
+			topOfStack--;
+			return res;
+		}
+
+	}
+	public int peek() {
+		if(isEmpty()) {
+			System.out.println("The stack is empty");
+			return -1;
+		}else {
+			return arr[topOfStack];
+		}
+	}
+	public void deleteStack() {
+		topOfStack=-1;
+	}
+
+}
