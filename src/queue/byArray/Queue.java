e8996334ee81ec2d920cc8983c90477ccd64b9a3
sum7615 <sum793763@gmail.com>
2023-04-21 17:28:06 +0530

    Creating queue

--- src/queue/byArray/Queue.java
+++ src/queue/byArray/Queue.java
@@ -1,5 +1,13 @@
 package queue.byArray;
 
 public class Queue {
+	public int arr[];
+	public int topOfQueue;
+	public int beginingofQueue;
+	public Queue(int size) {
+		this.arr= new int[size];
+		this.topOfQueue=-1;
+		this.beginingofQueue=-1;
+	}
 
 }
