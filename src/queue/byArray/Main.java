de0ffceb0893d1dc4c35dc3ddb29a38f9e23df45
sum7615 <sum793763@gmail.com>
2023-04-26 15:16:30 +0530

    linear queue bug fixing

--- src/queue/byArray/Main.java
+++ src/queue/byArray/Main.java
@@ -1,5 +1,23 @@
 package queue.byArray;
 
-public class Main {
+import queue.byArray.circular.CircularQueue;
 
+public class Main {
+	public static void main(String[] args) {
+		Queue qu = new Queue(4);
+		
+		qu.enQueue(5);
+		qu.enQueue(6);
+		qu.enQueue(7);
+		qu.enQueue(8);
+//
+//		qu.deQueue();
+//		qu.deQueue();
+//		qu.deQueue();
+		
+//		System.out.println(qu.peek());
+		
+		System.out.println(qu.toString());  
+	}
+	
 }
--- src/queue/byArray/Queue.java
+++ src/queue/byArray/Queue.java
@@ -1,45 +1,78 @@
 package queue.byArray;
 
+import java.util.Arrays;
+
 public class Queue {
 	public int arr[];
 	public int topOfQueue;
 	public int beginingofQueue;
+
 	public Queue(int size) {
-		this.arr= new int[size];
-		this.topOfQueue=-1;
-		this.beginingofQueue=-1;
+		this.arr = new int[size];
+		this.topOfQueue = -1;
+		this.beginingofQueue = -1;
 	}
+
 	public boolean isFull() {
-		if(topOfQueue==arr.length-1) {
-			return true;
-		}else {
-			return false;
-		}
+		return topOfQueue == arr.length - 1;
 	}
+
 	public boolean isEmpty() {
-		if(topOfQueue==-1) {
-			return true;
-		}else {
-			return false;
+		return topOfQueue == -1;
+	}
+
+	public void enQueue(int value) {
+		if (isFull()) {
+			System.out.println("Queue is full");
+		} else {
+			if (isEmpty()) {
+				System.out.println("Entering into empty queue");
+				beginingofQueue = 0;
+			}
+			topOfQueue += 1;
+			arr[topOfQueue] = value;
+			System.out.println("Added");
 		}
 	}
-	
-	
-	
-	public void enQueue(int value) {
-		if(isFull()) {
-			System.out.println("Queue is full");
-		}else if(isEmpty()) {
-			topOfQueue++;
-			beginingofQueue=0;
-			arr[topOfQueue]=value;
-			System.out.println("Added");
-			return;
-		}else {
-			topOfQueue++;
-			arr[topOfQueue]=value;
-			System.out.println("Added");
-			return;
+
+	public int deQueue() {
+		if (isEmpty()) {
+			System.out.println("No value");
+			return -1;
+		} else {
+			int res = arr[beginingofQueue];
+			arr[beginingofQueue] = Integer.MIN_VALUE;
+			if (beginingofQueue == topOfQueue) {
+				beginingofQueue = -1;
+				topOfQueue = -1;
+			} else {
+				beginingofQueue++;
+			}
+			return res;
 		}
+	}
+
+	public int peek() {
+		if (isEmpty()) {
+			System.out.println("No Value");
+			return -1;
+		} else {
+			return arr[beginingofQueue];
+		}
+	}
+
+	public void delete() {
+		if (isEmpty()) {
+			System.out.println("The queue is empty");
+		} else {
+			arr = null;
+		}
+	}
+
+	@Override
+	public String toString() {
+		topOfQueue = -1;
+		beginingofQueue = -1;
+		return Arrays.toString(arr);
 	}
 }
