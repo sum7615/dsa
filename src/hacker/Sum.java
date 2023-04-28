e15a349b4d4580270adcc1861363276b0b6fbe97
sum7615 <sum793763@gmail.com>
2023-04-28 23:06:16 +0530

    kufw

--- src/hacker/Sum.java
+++ src/hacker/Sum.java
@@ -35,6 +35,7 @@ public class Sum {
                 if(ints[i]!=null){
                      s+=ints[i];
                      
+                     
                 }
               
                 
