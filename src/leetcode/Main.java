2c7b3b040290a5ffd19cd2bb4b441e0103ce1196
sum7615 <sum793763@gmail.com>
2023-05-04 15:32:12 +0530

    kjbwdf

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -1,13 +1,13 @@
 package leetcode;
 
-import java.util.Arrays;
-import java.util.List;
+
 
 public class Main {
 	public static void main(String[] args) {
 
-		LongestCommonPrefix ll = new LongestCommonPrefix();
-		String[] strs = {"flower","cflar","flow","flight"};
-		System.out.println(ll.longestCommonPrefix(strs));
+		Parentheses par = new Parentheses();
+		String s = "([])";
+		System.out.println(par.isValid(s));
+
 	}
 }
---
+++ src/leetcode/Parentheses.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+
+
+public class Parentheses {
+	public boolean isValid(String s) {
+		Stack<Character> stack = new Stack<>();
+	        for (char c : s.toCharArray()) {
+	            if (c == '(' || c == '{' || c == '[') {
+	                stack.push(c);
+	            } else {
+	                if (stack.empty()) {
+	                    return false;
+	                }
+	                if (c == ')' && stack.peek() == '(') {
+	                    stack.pop();
+	                } else if (c == '}' && stack.peek() == '{') {
+	                    stack.pop();
+	                } else if (c == ']' && stack.peek() == '[') {
+	                    stack.pop();
+	                } else {
+	                    return false;
+	                }
+	            }
+	        }
+	        return stack.empty();
+	}
+}
