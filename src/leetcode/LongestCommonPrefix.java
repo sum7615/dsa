ff38a49f02e483f0f6de0d585e40938221ca9640
sum7615 <sum793763@gmail.com>
2023-05-08 23:25:45 +0530

    iugfa

--- src/leetcode/LongestCommonPrefix.java
+++ src/leetcode/LongestCommonPrefix.java
@@ -6,6 +6,7 @@ public class LongestCommonPrefix {
 	public String longestCommonPrefix(String[] strs) {
 		
 		Arrays.sort(strs);
+		
         String s1 = strs[0];
         String s2 = strs[strs.length-1];
         int idx = 0;
