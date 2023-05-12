2764bdea93df40f2525abca10eb46730d61892c5
sum7615 <sum793763@gmail.com>
2023-05-12 16:24:56 +0530

    Find time to bye ticket leetcode

--- src/stack/byLinkedList/leetcode/Main.java
+++ src/stack/byLinkedList/leetcode/Main.java
@@ -2,7 +2,10 @@ package stack.byLinkedList.leetcode;
 
 public class Main {
 	public static void main(String[] args) {
+		Ticket_Queue tq=new Ticket_Queue();
+		int arr[]= {2,3,2};
 		
+	 System.out.println(tq.timeRequiredToBuy(arr, 2));
 		
 	} 
 
---
+++ src/stack/byLinkedList/leetcode/Ticket_Queue.java
@@ -0,0 +1,40 @@
+package stack.byLinkedList.leetcode;
+
+import java.security.KeyPair;
+import java.security.interfaces.DSAKeyPairGenerator;
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class pair {
+	int key;
+	int value;
+	public pair(int key, int value) {
+		super();
+		this.key = key;
+		this.value = value;
+	}
+
+}
+
+public class Ticket_Queue {
+	public int timeRequiredToBuy(int[] tickets, int k) {
+		Queue<pair> queue = new ArrayDeque<>();
+		for (int i = 0; i < tickets.length; i++) {
+			queue.offer(new pair(i, tickets[i]));
+		}
+		int count = 0;
+		while (!queue.isEmpty()) {
+			pair temp = queue.poll();
+			count += 1;
+			if (temp.value > 1) {
+				queue.offer(new pair(temp.key, temp.value-1));
+			}else if(temp.value == 1 && temp.key == k) {
+				return count;
+			}
+
+		}
+		return count;
+	}
+}
