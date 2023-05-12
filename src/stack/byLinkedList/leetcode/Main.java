80cb5528c68150f7bae9c483a8de0bbd1857679e
sum7615 <sum793763@gmail.com>
2023-05-12 10:48:42 +0530

    Queue implementation using stack leetcode

--- src/stack/byLinkedList/leetcode/Main.java
+++ src/stack/byLinkedList/leetcode/Main.java
@@ -2,8 +2,15 @@ package stack.byLinkedList.leetcode;
 
 public class Main {
 	public static void main(String[] args) {
-
+		MyQueue myQueue = new MyQueue();
+		myQueue.push(1);
+		myQueue.push(2);
+		myQueue.push(3);
+		myQueue.print();
+		System.out.println();
+		System.out.println(myQueue.peek());
+		System.out.println();
+		myQueue.print();
 	}
-	
 
 }
--- src/stack/byLinkedList/leetcode/MyQueue.java
+++ src/stack/byLinkedList/leetcode/MyQueue.java
@@ -1,23 +1,54 @@
 package stack.byLinkedList.leetcode;
 
+import java.util.Stack;
+
 public class MyQueue {
+	Stack<Integer> stack;
 
 	public MyQueue() {
+		stack = new Stack<>();
 	}
 
 	public void push(int x) {
-
+		stack.push(x);
 	}
 
 	public int pop() {
-
+		int top = stack.pop();
+	    int result;
+	    if (stack.isEmpty()) {
+	        result = top;
+	    } else {
+	        result = pop();
+	        stack.push(top);
+	    }
+	    return result;
 	}
 
 	public int peek() {
-
+		int top = stack.pop();
+	    int result;
+	    if (stack.isEmpty()) {
+	        result = top;
+	        stack.push(top);
+	    } else {
+	        result = peek();
+	        stack.push(top);
+	    }
+	    return result;
 	}
 
 	public boolean empty() {
-
+		return stack.isEmpty();
+	}
+	
+	
+	public void print() {
+		for(int i : stack) {
+			System.out.print(i);
+			if(i<stack.size()) {
+				System.out.print("-");
+			}
+		}
 	}
 }
