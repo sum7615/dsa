aae8fd52f7b5a47a6e79ac9b9ab606ebacf2c65a
sum7615 <sum793763@gmail.com>
2023-05-12 11:28:31 +0530

    leetcode RecentCounter

--- src/stack/byLinkedList/leetcode/Main.java
+++ src/stack/byLinkedList/leetcode/Main.java
@@ -2,15 +2,15 @@ package stack.byLinkedList.leetcode;
 
 public class Main {
 	public static void main(String[] args) {
-		MyQueue myQueue = new MyQueue();
-		myQueue.push(1);
-		myQueue.push(2);
-		myQueue.push(3);
-		myQueue.print();
-		System.out.println();
-		System.out.println(myQueue.peek());
-		System.out.println();
-		myQueue.print();
+		RecentCounter rc = new RecentCounter();
+//		[1178],[1483],[1563],[4054],[4152]]
+		
+		System.out.println(rc.ping(1178));
+		System.out.println(rc.ping(1483));
+		System.out.println(rc.ping(1563));
+		System.out.println(rc.ping(4054));
+		System.out.println(rc.ping(4152));
+		
 	}
 
 }
