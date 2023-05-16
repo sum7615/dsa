7cef35118314dbfc924a5cdd68fdf86b7d1084e0
sum7615 <sum793763@gmail.com>
2023-05-16 11:07:19 +0530

    Merge two sorted list

--- src/leetcode/ListNode.java
+++ src/leetcode/ListNode.java
@@ -3,4 +3,17 @@ package leetcode;
 public class ListNode {
 	int value;
 	ListNode next;
+
+	public ListNode() {
+		// TODO Auto-generated constructor stub
+	}
+
+	ListNode(int value) {
+		this.value = value;
+	}
+
+	ListNode(int value, ListNode next) {
+		this.value = value;
+		this.next = next;
+	}
 }
--- src/leetcode/MergeTwoSortedList.java
+++ src/leetcode/MergeTwoSortedList.java
@@ -2,10 +2,31 @@ package leetcode;
 
 public class MergeTwoSortedList {
 	public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+		ListNode head1 = list1;
+		ListNode head2 = list2;
+
+		ListNode dummy = new ListNode(0);
+		ListNode current = dummy;
+
+		while (head1 != null && head2 != null) {
+			if (head1.value < head2.value) {
+				current.next = head1;
+				head1 = head1.next;
+			} else {
+				current.next = head2;
+				head2 = head2.next;
+			}
+			current = current.next;
+		}
+
+		if (head1 != null) {
+			current.next = head1;
+		} else {
+			current.next = head2;
+		}
+
+		return dummy.next;
+
 	}
 
-	
-
-
 }
-
