27e9d6dbcde564793b4622397c04ff141f107726
sum7615 <sum793763@gmail.com>
2023-05-19 20:18:12 +0530

    SetOfStacks

--- src/stack/byLinkedList/SetOfStacks.java
+++ src/stack/byLinkedList/SetOfStacks.java
@@ -8,6 +8,7 @@ public class SetOfStacks {
 	
 	
 	
+	
 	int subSTacksize;
 //	private List<Integer> elements = new ArrayList<Integer>();
 	private List<StackByArray> listOfStack = new ArrayList<StackByArray>();
