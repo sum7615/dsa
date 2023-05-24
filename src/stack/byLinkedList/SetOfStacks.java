f2ccfbb2192efb27ebeb62ff30bcd6e7321d0b54
sum7615 <sum793763@gmail.com>
2023-05-24 20:49:49 +0530

    SetOfStacks

--- src/stack/byLinkedList/SetOfStacks.java
+++ src/stack/byLinkedList/SetOfStacks.java
@@ -1,20 +1,14 @@
 package stack.byLinkedList;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
-
 public class SetOfStacks {
 	int subSTacksize;
 //	private List<Integer> elements = new ArrayList<Integer>();
 	private List<StackByArray> listOfStack = new ArrayList<StackByArray>();
 	public SetOfStacks(int subSTacksize) {
-		
 		this.subSTacksize = subSTacksize;
 		listOfStack.add(new StackByArray());
 	}
-
 	public void push(int value) {
 //		elements.add(value);
 		if (listOfStack.get(listOfStack.size() - 1).push(value)) {
